0466369dfe493d909c228e44ef5873b74436b1d6
efeckgz <efeckgz@gmail.com>
2023-05-03 08:57:33 +0300

    Created frame.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+
+        frame.setTitle("SpaceJavaders");
+        frame.setSize(700, 500);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
