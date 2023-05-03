7c70074aeecebc568bc156e0cabccf7fd5a9a4cf
efeckgz <efeckgz@gmail.com>
2023-05-03 11:25:15 +0300

    Early implementation of menu bar.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        // Use native macOS menu bar if possible
+        try {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+        } catch (Exception e) {
+            // Do nothing if the property cannot be set
+        }
 
-        frame.setTitle("SpaceJavaders");
-        frame.setSize(700, 500);
-        frame.setResizable(false);
+        JFrame frame = new JFrame("SpaceJavaders"); // Frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenuBar menuBar = new JMenuBar(); // Create the menu bar
+
+        // Creating & adding menus
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+
+        // Creating & adding menu items
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitItem);
+
+        // Setting frame properties
+        frame.setJMenuBar(menuBar);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 }
