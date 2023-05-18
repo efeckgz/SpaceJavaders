d05b797bacff4cbaa46ba843a0f4e81733b10069
efeckgz <efeckgz@gmail.com>
2023-05-18 17:59:36 +0300

    Wrote documentation for the Levels.

--- src/Levels.java
+++ src/Levels.java
@@ -1,4 +1,10 @@
 public class Levels {
+    // A class to hold the game levels. Each level is a two dimensional array. The first dimension is the
+    // rows at which the aliens are going to be spawned. In the second dimension, the aliens are represented
+    // by int values. Each different value represent a different type of alien
+    // (1 -> red, 2 -> green, 3 -> yellow, 4 -> extra, 0 -> no alien). This is how the levels for the original
+    // Space Invaders game was designed.
+
     private static final int[][] level1 = {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
